312ef2b96a94544f582c58f84dd7fa92bbc4da0c
GarotoNoto <78836028+GarotoNoto@users.noreply.github.com>
2023-01-31 16:04:26 -0600

    Crear archivo y pruba con 1M de datos

--- usaVector.java
+++ usaVector.java
@@ -1,3 +1,15 @@
 public class usaVector() {
-  public static  
+  public static void main(String[] args){
+
+    Vector vec = new Vector(1100000);
+
+    for(int i = 0; i < 10; i++){
+      vec.altaPos(i, (int)(Math.random()*1000));
+    }
+    System.out.println(vec);
+
+    vec.heapSort();
+    System.out.println("ORDENDADOS: ");
+    System.out.println(vec);
+  }
 }
