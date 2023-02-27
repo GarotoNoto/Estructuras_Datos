066815bd653f476608a0b4a1016e5b3839311996
GarotoNoto <josuegalindo014@gmail.com>
2023-02-26 20:19:50 -0600

    Agrega comentario

--- Queue_y_Calculadora/Queue.java
+++ Queue_y_Calculadora/Queue.java
@@ -2,42 +2,46 @@ package Queue_y_Calculadora;
 
 /*
     Es el ejemplo de una cola circular
+
+    EN Java hay un Queue ya establecido de base
+        Queue<String> cola = new LinkedList<>()
+    Se implementa en LinkedList y otros
  */
 
 public class Queue {
     int [] cola; //elementos de la cola
     int numero; // Tamaño
-    int front, rear;
+    int head, tail;
 
     //Constructores
     public Queue (int numero){
         this.numero = numero;
         cola = new int[numero];
-        front = 0;
-        rear = 0;
+        head = 0;
+        tail = 0;
     }
     public Queue() { this(4); }
 
-    public boolean vacia() { return front == rear; }
-    public boolean llena() { return (front+1) % numero == front; }
+    public boolean vacia() { return head == tail; }
+    public boolean llena() { return (head+1) % numero == head; }
 
     public boolean push (int elemento){
         if(llena())
             return false;
-        cola[rear] = elemento;
-        rear = (rear+1)%numero;
+        cola[tail] = elemento;
+        tail = (tail+1)%numero;
         return true;
     }
 
     public boolean pop(){
         if(vacia())
             return false;
-        front = (front+1)%numero;
+        head = (head+1)%numero;
         return true;
     }
 
     public int peek(){
-        return cola[front];
+        return cola[head];
     }
     /*
     @Override
