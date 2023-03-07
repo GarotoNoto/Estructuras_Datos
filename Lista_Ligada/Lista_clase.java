2442b1c5f6f5f04b59de809f7f46588912b189f8
GarotoNoto <josuegalindo014@gmail.com>
2023-03-07 15:42:24 -0600

    Cambio de nombres de variables y arreglo de espacios

--- Lista_Ligada/Lista_clase.java
+++ Lista_Ligada/Lista_clase.java
@@ -7,95 +7,82 @@ public class Lista_clase implements Interface{
         int dato;
         Nodo liga;
 
-        Nodo(int dato)
-        {
+        Nodo(int dato) {
             this.dato = dato;
             liga = null;
         }
     }
 
-    private Nodo L; //referencia al inicio de la lista ligada
-    private int nn; // numero de nodos en la lista ligada
+    private Nodo apuntador; //referencia al inicio de la lista ligada
+    private int numNodos; // numero de nodos en la lista ligada
 
-    public Lista_clase()
-    {
-        L = null; // indica lista ligada vacia
-        nn = 0; // la lista ligada vacia contiene 0 nodos
+    public Lista_clase() {
+        apuntador = null; // indica lista ligada vacia
+        numNodos = 0; // la lista ligada vacia contiene 0 nodos
     }
 
     @Override
-    public void altaIni(int ele)
-    {
+    public void altaIni(int ele) {
         Nodo nvo = new Nodo(ele); // crea un nuevo nodo
-        nvo.liga = L; // señala al dato siguiente
-        L = nvo; // se hace que L indique el nodo que acaba de
-        nn++; // indica el numero de nodos de la lista ligada
+        nvo.liga = apuntador; // señala al dato siguiente
+        apuntador = nvo; // se hace que L indique el nodo que acaba de
+        numNodos++; // indica el numero de nodos de la lista ligada
 
     }
 
     @Override
-    public void altaFin(int ele)
-    {
+    public void altaFin(int ele) {
 
     }
 
     @Override
-    public void altaPos(int ele, int pos)
-    {
+    public void altaPos(int ele, int pos) {
 
     }
 
     @Override
-    public boolean bajaIni()
-    {
+    public boolean bajaIni() {
         return false;
     }
 
     @Override
-    public boolean bajaFin()
-    {
+    public boolean bajaFin() {
         return false;
     }
 
     @Override
-    public boolean bajaPos(int pos)
-    {
+    public boolean bajaPos(int pos) {
         return false;
     }
 
     @Override
-    public int eleIni()
-    {
+    public int eleIni() {
         throw new UnsupportedOperationException("Not supported yet");
     }
 
     @Override
-    public int eleFin()
-    {
+    public int eleFin() {
         throw new UnsupportedOperationException("Not supported yet");
     }
 
     @Override
-    public int elePos(int pos)
-    {
+    public int elePos(int pos) {
         throw new UnsupportedOperationException("Not supported yet");
     }
 
     @Override
-    public boolean vacia()
-    {
+    public boolean vacia() {
         return false;
     }
 
     @Override
-    public String toString()
-    {
-        Nodo ptr = L; // referencia a un nodo de la lista ligada
+    public String toString() {
+        Nodo lista = apuntador; // referencia a un nodo de la lista ligada
         String sal = " ";
-        while(ptr != null)
+        while(lista != null)
         {
-            sal += ptr.dato + ", "; // agrega a sal el dato del
-            ptr = ptr.liga; // avanza el siguiente nodo de la lista ligada
+            sal += lista.dato + ", "; // agrega a sal el dato del
+            lista = lista.liga; // avanza el siguiente nodo de la lista ligada
         }
         return sal;
     }
