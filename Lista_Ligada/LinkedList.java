b9da0c2fd559eaae3b722df803e3cae44b6a5826
GarotoNoto <78836028+GarotoNoto@users.noreply.github.com>
2023-03-20 22:13:39 -0600

    Se creó el archivo

--- Lista_Ligada/LinkedList.java
+++ Lista_Ligada/LinkedList.java
@@ -1,1 +1,33 @@
+class LinkedList {
+    // Crea un nodo
+    Node head;
 
+    static class Node{
+        int value;
+        Node next;
+
+        Node(int d){
+            value = d;
+            next = null;
+        }
+    }
+
+    public static void main(String[] args){
+        LinkedList ListaLigada = new LinkedList();
+
+        //Asignando valores
+        ListaLigada.head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+
+        //Conectando Nodos
+        ListaLigada.head.next = second;
+        second.next = third;
+
+        //Imprimiendo valores
+        while (ListaLigada.head != null){
+            System.out.print(ListaLigada.head.value + " ");
+            ListaLigada.head = ListaLigada.head.next;
+        }
+    }
+}
